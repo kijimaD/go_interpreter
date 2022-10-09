4ff5062039974326810e2aa7388623b1160b380c
kijimaD <norimaking777@gmail.com>
2022-10-09 19:35:11 +0900

    feat: more keywords

--- token/token.go
+++ token/token.go
@@ -11,7 +11,7 @@ const (
 	ILLEGAL = "ILLEGAL"
 	EOF     = "EOF"
 
-	// 識別子 + リテラル
+	// 識別子 + リテラル。数値や変数名など、予約語ではないもの。
 	IDENT = "IDENT"
 	INT   = "INT"
 
@@ -37,13 +37,25 @@ const (
 	// キーワード
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
 
+// 予約語
 var keywords = map[string]TokenType{
-	"fn":  FUNCTION,
-	"let": LET,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
 }
 
+// 予約語の場合はその種類を、それ意外の場合はIDENTを返す
 func LookupIdent(ident string) TokenType {
 	if tok, ok := keywords[ident]; ok {
 		return tok
