a8449e65739853cc2d12b9c264c3c049fa744ad8
kijimaD <norimaking777@gmail.com>
2022-10-11 09:37:16 +0900

    feat: return statement

--- parser/parser.go
+++ parser/parser.go
@@ -71,6 +71,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -93,6 +95,20 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 	if !p.expectPeek(token.ASSIGN) {
 		return nil
 	}
+
+	// TODO: セミコロンに遭遇するまで式を読み飛ばしてしまっている
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+// returnをパースする
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+
+	p.nextToken()
 
 	// TODO: セミコロンに遭遇するまで式を読み飛ばしてしまっている
 	for !p.curTokenIs(token.SEMICOLON) {
