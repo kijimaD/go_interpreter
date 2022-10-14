c10b5f3999b1641b057bb682ed8082b4a19a4538
kijimaD <norimaking777@gmail.com>
2022-10-14 10:29:39 +0900

    feat: evaluate boolean

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,19 +5,25 @@ import (
 	"monkey/object"
 )
 
+var (
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 
 	// 文
 	case *ast.Program:
 		return evalStatements(node.Statements)
-
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
 
 	// 式
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
 	}
 
 	return nil
@@ -31,4 +37,12 @@ func evalStatements(stmts []ast.Statement) object.Object {
 	}
 
 	return result
+}
+
+// 毎回新しいobject.Booleanを生成するのではなく、一度生成した参照を使う。これによってobject.Booleanのインスタンスは2つだけになり性能が向上する
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
 }
