53ea00b77bb394b4eefab8db629876ecef77feef
kijimaD <norimaking777@gmail.com>
2022-10-14 09:37:15 +0900

    feat: null object

--- object/object.go
+++ object/object.go
@@ -11,6 +11,7 @@ type ObjectType string
 const (
 	INTEGER_OBJ = "INTEGER"
 	BOOLEAN_OBJ = "BOOLEAN"
+	NULL_OBJ    = "NULL"
 )
 
 // Monkeyソースコードを評価する際に出てくる値全てをObjectで表現する。全ての値はObjectインターフェースを満たす構造体にラップされる
@@ -32,4 +33,9 @@ type Boolean struct {
 }
 
 func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
-func (b *Boolean) Inspect() string  { return fmt.SPrintf("%d", b.Value) }
+func (b *Boolean) Inspect() string  { return fmt.Sprintf("%d", b.Value) }
+
+type Null struct{}
+
+func (n *Null) Type() ObjectType { return NULL_OBJ }
+func (n *Null) Inspect() string  { return "null" }
