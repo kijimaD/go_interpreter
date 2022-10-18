9cecbe2e3c1b0856af8aab31c075a9e69b0f9f57
kijimaD <norimaking777@gmail.com>
2022-10-18 12:05:07 +0900

    feat: unless macro

--- repl/repl.go
+++ repl/repl.go
@@ -19,6 +19,7 @@ const MONKEY_FACE = `
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
 	env := object.NewEnvironment()
+	macroEnv := object.NewEnvironment()
 
 	for {
 		fmt.Printf(PROMPT)
@@ -37,7 +38,10 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program, env)
+		evaluator.DefineMacros(program, macroEnv)
+		expanded := evaluator.ExpandMacros(program, macroEnv)
+
+		evaluated := evaluator.Eval(expanded, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
